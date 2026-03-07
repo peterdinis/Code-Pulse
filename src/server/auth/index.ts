4c80456f8038aad0e1a49d866a1a33e95f6cced3
peterdinis <petter1@pobox.sk>
2026-03-07 18:17:03 +0100

    infra: Adding option for github login WIP

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -6,4 +6,24 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "sqlite",
   }),
+  emailAndPassword: {
+    enabled: true
+  },
+  socialProviders: {
+    github: {
+        clientId: "",
+        clientSecret: ""
+    }
+  },
+  session: {
+    expiresIn: 60 * 60 * 24 * 7,
+    updateAge: 60 * 60 * 24,
+    cookieCache: {
+        enabled: true,
+        maxAge: 60 * 5 
+    }
+  },
+  trustedOrigins: ["http://localhost:3000"]
 });
+
+export type Session = typeof auth.$Infer.Session;
